754f0e689382c577fa3491cfd968ac04877af66e
kipash-prog <kidus.ugr-0110-15@aau.edu.et>
2025-07-23 19:11:36 +0300

    custom admin

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Add your templates directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
